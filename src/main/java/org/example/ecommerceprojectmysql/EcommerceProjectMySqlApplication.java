91f3495c49368c9bac3d58dc797dac803f2f52a5
Siddarth Banerjee <siddarth2801@gmail.com>
2026-05-22 17:10:36 +0530

    initial commit, added model for the categories

---
+++ src/main/java/org/example/ecommerceprojectmysql/EcommerceProjectMySqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecommerceprojectmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProjectMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProjectMySqlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/ecommerceprojectmysql/model/Category.java
@@ -0,0 +1,40 @@
+package org.example.ecommerceprojectmysql.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "active", columnDefinition = "BOOLEAN DEFAULT true")
+    private Boolean active = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
