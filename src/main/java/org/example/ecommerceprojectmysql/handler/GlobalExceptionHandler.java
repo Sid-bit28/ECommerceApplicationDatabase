ae27e0b0bbf729ccbc2c23ecf682e5dfac753cf3
Siddarth Banerjee <siddarth2801@gmail.com>
2026-05-24 13:10:53 +0530

    added InvalidOperationExceptionHandler, DuplicationResourceExceptionHandler, MethodArgumentInvalidExceptionHandler and generic ExceptionHandler in GlobalExceptionHandler

--- src/main/java/org/example/ecommerceprojectmysql/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/ecommerceprojectmysql/handler/GlobalExceptionHandler.java
@@ -1,15 +1,21 @@
 package org.example.ecommerceprojectmysql.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.ecommerceprojectmysql.exception.DuplicateResourceException;
 import org.example.ecommerceprojectmysql.exception.ErrorResponse;
+import org.example.ecommerceprojectmysql.exception.InvalidOperationException;
 import org.example.ecommerceprojectmysql.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 @Slf4j
@@ -27,5 +33,62 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateResourceException(DuplicateResourceException ex, WebRequest request) {
+        log.warn("Duplicate resource: {}", ex.getMessage());
+        String path = request.getDescription(false).replace("uri=", "");
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .path(path)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
+    @ExceptionHandler(InvalidOperationException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidOperationException(InvalidOperationException ex, WebRequest request) {
+        log.warn("Invalid operation: {}", ex.getMessage());
+        String path = request.getDescription(false).replace("uri=", "");
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .message(ex.getMessage())
+                .path(path)
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        log.warn("Method argument not valid: {}", ex.getMessage());
+        String path = request.getDescription(false).replace("uri=", "");
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .path(path)
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .errors(errors)
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex, WebRequest request) {
+        log.error("Exception:", ex);
+        String path = request.getDescription(false).replace("uri=", "");
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .path(path)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An error occurred processing your request")
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
 }
